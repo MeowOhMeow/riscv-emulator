91d693d92aa6f762ee97c7bbe1df00a19d64d0b9
MeowOhMeow <abc22681157@gmail.com>
2023-10-21 10:22:03 +0800

    implement jalr

--- opcodes/I_type_3/jalr.h
+++ opcodes/I_type_3/jalr.h
@@ -9,6 +9,8 @@
 
 static inline void execute_JALR(inst_t inst)
 {
+    cpu.GPR[inst.I_TYPE.rd] = cpu.PC + 4;
+    cpu.PC = (cpu.GPR[inst.I_TYPE.rs1] + sext(inst.I_TYPE.imm_11_0, 12)) & ~0x1;
 }
 
 #endif // JALR_H
