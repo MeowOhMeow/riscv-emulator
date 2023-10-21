ad53cb80f1ee239795a275e85bb8008065da16e6
MeowOhMeow <abc22681157@gmail.com>
2023-10-21 12:29:19 +0800

    fix wrong opcode const

--- opcode.h
+++ opcode.h
@@ -3,8 +3,9 @@
 
 #define OP_I_TYPE_1 0b0010011
 #define OP_I_TYPE_2 0b0000011
-#define OP_I_TYPE_3 0b1110011
-#define OP_I_TYPE_4 0b1100111
+#define OP_I_TYPE_3 0b1100111
+#define OP_I_TYPE_4 0b1110011
+
 #define OP_R_TYPE 0b0110011
 
 #include "opcodes/i_type_1.h"
